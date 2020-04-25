c18697635c4d269a4c650561ff6b01b7947f3793
Alexander Vieth <aovieth@gmail.com>
2020-04-25 19:02:01 -0400

    sensors calibrated, filtered, looking good

--- arduinobits/pressure_sensors.h
+++ arduinobits/pressure_sensors.h
@@ -1,20 +1,19 @@
-/*
-   20200328
-   author: Colin Gallacher
-   v0.1
-
-*/
-
-
 #define AREA1                0.0003976
 #define AREA2                0.000044156
 #define AIR_DENSITY          1.225
 #define DENOMINATOR()        (((AREA1/AREA2) * (AREA1/AREA2)) - 1)
 
+/* Remember to set the analog read resolution to 12 bits */
+#define ADC_BITS 12
+/* mV reference voltage */
+#define ADC_REFERENCE 3300
+
 uint16_t sensor_offset(uint8_t analogPin);
-uint16_t read_sensor(uint8_t pin);
+
 float pressure_difference(uint16_t measurement);
 float flow_rate(uint16_t measurement);
+
+uint32_t pressure_difference_i(uint16_t measurement);
 
 /**
  * Reads a pin 10 times, takes the integral average plus 1, to give a baseline reading.
@@ -23,49 +22,59 @@ uint16_t sensor_offset(uint8_t analogPin){
   // int is wide enough, since we're only summing 10 measurements in [0,1024)
   // But oh well use 32 bits anyway.
   uint32_t sum = 0;
-  
-  for(int i = 0; i < 60; i++){ //blocking for initialization 
+
+  for(int i = 0; i < 60; i++){
     sum = sum + (uint32_t) (analogRead(analogPin));
   }
   sum = sum / 60;
-  sum += 4;
-  
-  return sum;
-}
 
-/**
- * Read the sensor on a given pin. Just a simple analogRead. No adjustment is done.
- * Resolution is hardware dependent. 10 bits on the arduino uno.
- */
-uint16_t read_sensor(uint8_t pin){
-  return analogRead(pin); 
+  return sum;
 }
 
 /**
  * The measurement comes from a sensor pin and is assumed to already have been corrected for signal offset.
  * 
- * TODO do integral computation. Values of magnitude less than 0.01 are discarded anyway.
- * Given that the measurement is in [0,1024), we have a lot of room to do non-floating-point arithmetic.
- * Instead of multiplying by 4.88 and dividing by 450, for example, we can put the measurement into a
- * 32-bit unsigned integer and multiply by 1000. The returned value would be 1000 times the pressure difference.
+ * TODO make it integral, pascals.
+ * How?
+ * 1. compute everything with 12 fractional parts.
  */
 float pressure_difference(uint16_t measurement){
-  float diff2mv = ((float) measurement) * 0.725;
-  float pressureDiff = 11.11 * 2.97 * (diff2mv / 1000.0);
-  // Unit is kPa.
-  return ((pressureDiff < 0.005) ? 0.0 : pressureDiff);
+  /* Convert bits to volts. */
+  float vs = (((float) measurement) / 4096.0) * 3.3;
+  /* From the sensor data sheet: convert that voltage to mmh2o */
+  //float diffmmh2o = vs * (1019.78 / 2.97);
+  // Experimentally-determined it to be 2.673
+  float diffmmh2o = vs * (1019.78 / 2.673);
+  /* Give cm h2o */
+  return (1.05 * diffmmh2o / 10.0);
+}
+
+/**
+ * Approximate pressure difference in pascals, multiplied by 2^16.
+ * It's derived from the floating point pressure_difference: compute the
+ * coefficient manually, multiply it by 2^16 and drop the fractional part, to
+ * get 207420.
+ */
+uint32_t pressure_difference_i(uint16_t measurement) {
+  return 207420 * (uint32_t) measurement;
 }
 
 /**
  * Liters per minute.
  *
  * FIXME can we get a good approx of this with only integers?
+ *
+ * Q = A_1 * sqrt((2 / ro) * (p / ((A_1/A_2)^2 - 1))
  * 
+ * where ro is air density, p is pressure difference  in pascals.
  */
 float flow_rate(uint16_t measurement) {
   float pressure = pressure_difference(measurement);
-  float numerator = 2000.0 * pressure / AIR_DENSITY;
+  // Got to make it pascals.
+  pressure = pressure * 98.0665;
+  float numerator = 2.0 * pressure / AIR_DENSITY;
   // numerator guaranteed to be positive since pressure is.
+  // m^3 / s
   float flow = AREA1 * sqrt(numerator/DENOMINATOR());
   // Litres per minute
   flow = flow * 60000.0;
--- arduinobits/sensor_control.h
+++ arduinobits/sensor_control.h
@@ -1,14 +1,6 @@
-/*
-   20200328
-   author: Colin Gallacher
-   v0.1
-
-*/
-
 #include "pins.h"
 #include "pressure_sensors.h"
-
-#define PASCAL_PER_CM_H2O 98.0665
+#include "median.h"
 
 #define SENSOR_COUNT 4
 /**
@@ -21,11 +13,43 @@
 
 uint16_t sensor_offsets[SENSOR_COUNT] = { 0, 0, 0, 0 };
 uint8_t sensor_pins[SENSOR_COUNT] = { 
-    PIN_INSP_PRESSURE,
-    PIN_INSP_FLOW,
-    PIN_EXP_FLOW,
-    PIN_AIR_IN_FLOW
-  };
+  PIN_INSP_PRESSURE,
+  PIN_INSP_FLOW,
+  PIN_EXP_FLOW,
+  PIN_AIR_IN_FLOW
+};
+
+static struct median_16 insp_pressure_median = {
+  .value = 0,
+  .index = 0,
+  .observation_order = { 0x00 },
+  .magnitude_order = { 0x00 }
+};
+static struct median_16 insp_flow_median = {
+  .value = 0,
+  .index = 0,
+  .observation_order = { 0x00 },
+  .magnitude_order = { 0x00 }
+};
+static struct median_16 exp_flow_median = {
+  .value = 0,
+  .index = 0,
+  .observation_order = { 0x00 },
+  .magnitude_order = { 0x00 }
+};
+static struct median_16 air_in_flow_median = {
+  .value = 0,
+  .index = 0,
+  .observation_order = { 0x00 },
+  .magnitude_order = { 0x00 }
+};
+
+struct median_16 *sensor_medians[SENSOR_COUNT] = {
+  &insp_pressure_median,
+  &insp_flow_median,
+  &exp_flow_median,
+  &air_in_flow_median
+};
 
 /**
  * Only call with one of the defined sensor names above.
@@ -33,30 +57,36 @@ uint8_t sensor_pins[SENSOR_COUNT] = {
  * 
  * The readings are corrected for the offset of that sensor (determined
  * by initializeSensors. If the reading is below the offset, you get a
- * negative number. Use read_sensor_with_offset_nonnegative if you don't
- * want that.
- * 
+ * negative number.
  */
 uint16_t read_sensor_with_offset(uint8_t sensor_id) {
   if (sensor_id < SENSOR_COUNT) {
-    uint16_t reading = read_sensor(sensor_pins[sensor_id]);
+    uint16_t reading = analogRead(sensor_pins[sensor_id]);
     uint16_t offset = sensor_offsets[sensor_id];
-    if (reading < offset) {
-      return 0;
-    } else {
-      return reading - offset;
-    }
+    uint16_t corrected = (reading < offset) ? 0 : reading - offset;
+    return corrected;
   } else {
     return 0;
   }
 }
 
 /**
- * read_sensor_with_offset but nonnegative.
+ * Update the filter for this sensor, and return the filtered value.
+ * Use the output of read_sensor_with_offset for the same sensor id.
+ *
+ * Gives 0 if the sensor id is invalid.
+ *
+ * Should call this regularly for all sensors, to keep the filters up to
+ * date.
+ * TODO could include timestamps in the filters.
  */
-uint16_t read_sensor_with_offset_nonnegative(uint8_t sensor_id) {
-  int16_t reading = read_sensor_with_offset(sensor_id);
-  return (reading < 0) ? 0 : ((uint16_t) reading);
+uint16_t get_update_filter(uint8_t sensor_id, uint16_t value) {
+  if (sensor_id < SENSOR_COUNT) {
+    median_16_step(sensor_medians[sensor_id], value);
+    return sensor_medians[sensor_id]->value;
+  } else {
+    return 0;
+  }
 }
 
 /**
@@ -68,7 +98,18 @@ int32_t smooth(int32_t x, int32_t y, int32_t z) {
 
 //INITIALIZATION 
 
+/**
+ * Set the read resolution to 12 bits, set the analog reference to 3.3
+ *
+ * TODO can we detect when the offset readings are abnormally high? What
+ * qualifies as too high? Probably 256 or greater I'd imagine...
+ */
 void initializeSensors(){
+  /* DUE only supports 3.3V. Put this call here just to be sure: if you try
+   * to build this for a non-DUE you will probably get an error because
+   * AR_DEFAULT is not defined
+   */
+  analogReference(AR_DEFAULT);
   analogReadResolution(12);
   sensor_offsets[INSPPRESSURE] = sensor_offset(sensor_pins[INSPPRESSURE]);
   sensor_offsets[INSPFLOW]     = sensor_offset(sensor_pins[INSPFLOW]);
@@ -79,13 +120,14 @@ void initializeSensors(){
 //SENSORS
 
 /**
- * Inspiration pressure at this instant, kPa (most information is in the fractional part).
+ * Inspiration pressure at this instant.
  * TODO make it integral.
  * TODO delete, we have an integral version.
  */
 float get_insp_pressure() {
   uint16_t rawData = read_sensor_with_offset(INSPPRESSURE);
-  return pressure_difference(rawData);
+  uint16_t filtered = get_update_filter(INSPPRESSURE, rawData);
+  return pressure_difference(filtered);
 }
 
 /**
@@ -94,8 +136,8 @@ float get_insp_pressure() {
  */
 float get_insp_flow() {
   uint16_t rawData = read_sensor_with_offset(INSPFLOW);
-  float r = flow_rate(rawData);
-  return r;
+  uint16_t filtered = get_update_filter(INSPFLOW, rawData);
+  return flow_rate(filtered);
 }
 
 /**
@@ -104,8 +146,8 @@ float get_insp_flow() {
  */
 float get_exp_flow() {
   uint16_t rawData = read_sensor_with_offset(EXPFLOW);
-  float r = flow_rate(rawData);
-  return r;
+  uint16_t filtered = get_update_filter(EXPFLOW, rawData);
+  return flow_rate(filtered);
 }
 
 /**
@@ -114,8 +156,8 @@ float get_exp_flow() {
  */
 float get_air_in_flow() {
   uint16_t rawData = read_sensor_with_offset(AIRINFLOW);
-  float r = flow_rate(rawData);
-  return r;
+  uint16_t filtered = get_update_filter(AIRINFLOW, rawData);
+  return flow_rate(filtered);
 }
 
 //COMPUTED VOLUMES
